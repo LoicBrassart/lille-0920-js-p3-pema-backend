5217b43f9016bf5aa544c4d0741036989c9863a9
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-27 09:33:07 +0100

    Add a get route

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const { port } = require("./conf.js")
+const { port } = require("./conf.js");
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    console.log("GET / was called!")
-    res.send("Hi there!");
+  console.log("GET / was called!");
+  res.send("Hi there!");
 });
 
 app.listen(port, () => {
-    console.log(`API avalable on http://localhost:${port}`)
-})
+  console.log(`API avalable on http://localhost:${port}`);
+});
+
+app.use("/timeline", require("./routes/timeline"));
