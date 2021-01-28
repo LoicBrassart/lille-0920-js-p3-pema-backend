9a6e875ba908d8df51d4190baeb92d0da5a880a8
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-28 14:26:17 +0100

    Add routes

--- routes/timeline.js
+++ routes/timeline.js
@@ -1,14 +1,67 @@
-const { port } = require("../conf");
+const { db } = require("../conf");
 const express = require("express");
 const router = express.Router();
 
-router.get("/getTimeline", async (req, res) => {
-  let timeline;
+router.get("/timeline", async (req, res) => {
+  let timelines;
   try {
-    timeline = await port.query(`SELECT * from timeline`);
+    timelines = await db.query(`SELECT * from timeline`);
   } catch (error) {
     res.status(500).send("Nope, sorry, I iz broken !");
     console.log(error);
   }
+  return res.json(timelines[0]);
+});
+
+router.get("/timeline/:id", async (req, res) => {
+  let timeline;
+  const id = req.params.id;
+  try {
+    timeline = await db.query(
+      `SELECT id, title, paragraphe from timeline WHERE id=?`,
+      [id]
+    );
+  } catch (err) {
+    return res.status(500).send("Nope, sorry, I iz broken !");
+  }
+  console.log(timeline[0]);
   return res.json(timeline[0]);
 });
+
+router.post("/timeline", async (req, res) => {
+  try {
+    const response = await db.query(`INSERT INTO timeline SET ?`, [req.body]);
+    res.status(201).send(req.body);
+    console.log(req.body);
+  } catch (err) {
+    res.status(500).send("Nope, sorry, I iz broken !", err);
+  }
+});
+
+router.delete("/timeline/:id", async (req, res) => {
+  let timeline;
+  const id = req.params.id;
+  console.log(id);
+  try {
+    timeline = await db.query(`DELETE FROM timeline WHERE id=?`, [id]);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Nope, sorry, I iz broken !");
+  }
+  return res.status(201).send("deleted successfully");
+});
+
+router.put("/timeline/:id", async (req, res) => {
+  const id = req.params.id;
+  const newTimeline = req.body;
+  console.log(req.body);
+  try {
+    await db.query(`UPDATE timeline SET ? WHERE id=?`, [newTimeline, id]);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Nope, sorry, I iz broken !");
+  }
+  return res.status(201).send("updtate successfully");
+});
+
+module.exports = router;
