5127a85189096dcbd36f8e38d69cfe9160341e99
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-28 14:25:56 +0100

    Rename index.js file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const { port } = require("./conf");
 const express = require("express");
+const cors = require("cors");
+const { port } = require("./conf");
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   console.log("GET / was called!");
@@ -11,4 +15,4 @@ app.listen(port, () => {
   console.log(`API avalable on http://localhost:${port}`);
 });
 
-app.use("/timeline", require("./routes/timeline"));
+app.use("/", require("./routes/timeline"));
