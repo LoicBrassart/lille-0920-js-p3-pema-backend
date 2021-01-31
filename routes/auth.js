050d3ce7fed3051c78fa6efa6729faaa44cca3c5
Sarahshr <saraah.sahraoui@gmail.com>
2021-01-31 18:16:51 +0100

    Add sql2/promise

--- routes/auth.js
+++ routes/auth.js
@@ -7,18 +7,18 @@ const jwt = require("jsonwebtoken");
 require("../passport-strategies");
 
 router.post("/signup", async (req, res) => {
-  const formData = req.body;
-  formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
-  db.query("INSERT INTO users SET ?", formData, (errSql, resSql) => {
-    if (!errSql) {
-      delete formData.password;
-      formData.id = resSql.insertId;
-      const token = jwt.sign(formData, jwt_secret);
-      res.status(201).json(token);
-    } else {
-      res.status(500).json(errSql);
-    }
-  });
+  try {
+    const formData = req.body;
+    formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
+    const [resSql] = await db.query("INSERT INTO users SET ?", formData);
+    delete formData.password;
+    formData.id = resSql.insertId;
+    const token = jwt.sign(formData, jwt_secret);
+    res.status(201).json(token);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
 });
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
