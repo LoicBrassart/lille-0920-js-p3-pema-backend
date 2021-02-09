5d36beca17355cc85d8f399fc2c233d16dd226b3
Sarahshr <saraah.sahraoui@gmail.com>
2021-02-09 16:08:03 +0100

    Add auth route

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const passport = require("passport");
 const { backPort } = require("./conf");
 
 const app = express();
 app.use(express.json());
+app.use(passport.initialize());
 app.use(cors());
 
 app.listen(backPort, () => {
   console.log(`API avalable on http://localhost:${backPort}`);
 });
+
+app.use("/auth", require("./routes/auth"));
 
 app.use("/admin", require("./routes/timeline"));
 
